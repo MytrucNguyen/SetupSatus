befcf7e08825e160dc2a82dd12b271e1eb8609f7
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-06-22 20:55:47 -0700

    Update SetupStatus.jsx

--- src/pages/SetupStatus/SetupStatus.jsx
+++ src/pages/SetupStatus/SetupStatus.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-// import React, { useState } from "react";
 import style from "./SetupStatus.module.css";
-// import data from "../../data/data.json";
 import { steps } from "../../data/data";
 
 const SetupStatus = () => {
@@ -71,67 +69,6 @@ const SetupStatus = () => {
       ))}
     </div>
   );
-
-  // If Json
-  // const isCompleted = useState(data.map((x) => false));
-
-  // const checkingStepCompletion = (data) => {
-  //   for (let i = 0; i < data.length; i++) {
-  //     if (data[i].status === "complete") {
-  //       isCompleted[i] = true
-  //     } else {
-  //       isCompleted[i] = false
-  //     }
-  //   }
-
-  // };
-
-  // checkingStepCompletion(data);
-
-  // return (
-  //   <div className={style.SetupStatusWrapper}>
-  //     {data.map((step, index) => (
-  //       <div
-  //         key={index}
-  //         className={
-  //           index !== data.length - 1 ? style.StatusBar : style.EndStatusBar
-  //         }
-  //       >
-  //         <div className={style.StatusText}>
-  //           <div
-  //             className={
-  //               isCompleted[index]
-  //                 ? style.StatusCircleCurrent
-  //                 : style.StatusCircleNotCurrent
-  //             }
-  //           >
-  //             {isCompleted[index] ? (
-  //                 <span className={style.Checkmark}>&#10003;</span>
-  //                 ) : (
-  //                 <>{index + 1}</>
-  //             )}
-  //           </div>
-  //           <div
-  //             className={
-  //               isCompleted[index]
-  //                 ? style.StatusNameCurrent
-  //                 : style.StatusNameNotCurrent
-  //             }
-  //           >
-  //             {step.name}
-  //           </div>
-  //         </div>
-  //         <div className={style.StatusBarLineCurrent}>
-  //           <hr
-  //             className={
-  //               isCompleted[index] ? style.ProcessBar : style.Inactive
-  //             }
-  //           />
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 };
 
 export default SetupStatus;
